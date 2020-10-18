ca2919e7e4ddd8a8e171ee94fa6849c5d95a5dd2
Gökberk AKDENİZ <gokberkakdeniz@hotmail.com>
2020-10-18 16:23:03 +0300

    refactor: handle message values with type function

--- src/messages.ts
+++ src/messages.ts
@@ -1,22 +1,35 @@
 /* eslint-disable no-prototype-builtins */
 
+export interface MessageParams {
+  [key: string]: unknown;
+}
+
+export interface MessageDict {
+  [key: string]: string | ((params: MessageParams) => string);
+}
+
 const messages = {
   "errors.unexpected": "Beklemeyen bir hata oluştu.",
   "errors.access_denied": "Erişim reddedildi.",
   "validation.missing_email_or_name": "Eposta ve şifre girilmeli.",
   "validation.invalid_email_or_name": "Eposta veya şifre geçersiz.",
-  "validations.maxlength": (name: string, length: number) =>
+  "validations.max_length": ({ name, length }: MessageParams) =>
     `${name} ${length} karakterden uzun olmamalı.`,
-  "validations.minlength": (name: string, length: number) =>
+  "validations.min_length": ({ name, length }: MessageParams) =>
     `${name} ${length} karakterden kısa olmamalı.`,
-  success: "Başarılı",
-  "success.updated": "Başarıyla güncellendi!",
-  "success.added": "Başarıyla eklendi!",
-  "success.removed": "Başarıyla silindi!",
+  "success.updated": "Başarıyla güncellendi.",
+  "success.added": "Başarıyla eklendi.",
+  "success.removed": "Başarıyla silindi.",
 } as const;
 
 export type Message = keyof typeof messages;
 
-const $ = (message: Message): string => messages[message];
+const $ = (message: Message, params?: MessageParams): string => {
+  const value = messages[message];
+
+  if (typeof value === "function") return value(params);
+
+  return value;
+};
 
 export default $;
