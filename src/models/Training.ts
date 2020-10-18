5914a0507e6cdf6c349b64ed1e29d3b2723ab7f1
Gökberk AKDENİZ <gokberkakdeniz@hotmail.com>
2020-10-18 16:19:39 +0300

    feat: add training model

--- src/models/Training.ts
+++ src/models/Training.ts
@@ -1,9 +1,34 @@
-import { Document } from "mongoose";
+import { Document, model, Schema } from "mongoose";
 import { UserRole } from "./User";
 
 export interface ITraining extends Document {
   title: string;
-  text: string;
-  video: string;
+  description: string;
+  media?: string[];
+  tags?: string[];
   type: UserRole;
 }
+
+const trainingSchema = new Schema<ITraining>({
+  title: {
+    type: String,
+    maxlength: [30, "Başlık 100 karakterden uzun olmamalı."],
+    required: [true, "Lütfen başlığı giriniz."],
+  },
+  description: {
+    type: String,
+    required: [true, "Lütfen tarif giriniz."],
+  },
+  tags: {
+    type: [String],
+    default: [],
+  },
+  media: {
+    type: [String],
+    default: [],
+  },
+});
+
+const trainingModel = model<ITraining>("Training", trainingSchema);
+
+export default trainingModel;
