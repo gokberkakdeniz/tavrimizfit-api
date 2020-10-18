13cfe256ed9d22231387a3f5967e3f8888dad93e
hakanalp <hakana926@hotmail.com>
2020-10-18 16:41:09 +0300

    feat: message types

--- src/messages.ts
+++ src/messages.ts
@@ -4,15 +4,12 @@ export interface MessageParams {
   [key: string]: unknown;
 }
 
-export interface MessageDict {
-  [key: string]: string | ((params: MessageParams) => string);
-}
-
 const messages = {
   "errors.unexpected": "Beklemeyen bir hata oluştu.",
   "errors.access_denied": "Erişim reddedildi.",
-  "validation.missing_email_or_name": "Eposta ve şifre girilmeli.",
-  "validation.invalid_email_or_name": "Eposta veya şifre geçersiz.",
+  "validations.invalid_email_or_name": "Eposta veya şifre geçersiz.",
+  "validations.missing_info": ({ name }: MessageParams) =>
+    `Lütfen ${name} giriniz.`,
   "validations.max_length": ({ name, length }: MessageParams) =>
     `${name} ${length} karakterden uzun olmamalı.`,
   "validations.min_length": ({ name, length }: MessageParams) =>
